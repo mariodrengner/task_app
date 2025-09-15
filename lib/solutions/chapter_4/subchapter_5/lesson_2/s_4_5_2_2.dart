1c11aabe02f3168f048e5529c4f576fcd06f5061
Mario Drengner <mario@drengner.de>
2025-09-15 11:47:08 +0200

    fix CardThemeData
    solved task 4.5.1.1 solved task 4.5.2.2

--- lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_2.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_2.dart
@@ -4,6 +4,22 @@ class S4522 extends StatelessWidget {
   const S4522({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return const MyStatelessWidget();
+  }
+}
+
+class MyStatelessWidget extends StatelessWidget {
+  const MyStatelessWidget({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        Text('Hello Mario'),
+        ElevatedButton(
+          onPressed: null,
+          child: Text('Klick mich'),
+        ),
+      ],
+    );
   }
 }
