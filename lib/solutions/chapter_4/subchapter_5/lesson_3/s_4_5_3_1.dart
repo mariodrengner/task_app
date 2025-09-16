6939568490a14e5e3284ecbc52516e276587e1c0
Mario Drengner <mario@drengner.de>
2025-09-16 13:24:14 +0200

    solve tasks (4.5.3.*)

--- lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_1.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_1.dart
@@ -4,6 +4,32 @@ class S4531 extends StatelessWidget {
   const S4531({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return const MyWidget();
+  }
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          color: Colors.blueGrey,
+          height: 100,
+          width: 100,
+        ),
+        Container(
+          color: Colors.blue,
+          height: 100,
+          width: 100,
+        ),
+        Container(
+          color: Colors.cyan,
+          height: 100,
+          width: 100,
+        ),
+      ],
+    );
   }
 }
--- lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_5.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_5.dart
@@ -4,6 +4,39 @@ class S4535 extends StatelessWidget {
   const S4535({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              color: Colors.red,
+              height: 100,
+              width: 100,
+            ),
+            Container(
+              color: Colors.green,
+              height: 100,
+              width: 100,
+            ),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              color: Colors.blue,
+              height: 100,
+              width: 100,
+            ),
+            Container(
+              color: Colors.yellow,
+              height: 100,
+              width: 100,
+            ),
+          ],
+        ),
+      ],
+    );
   }
 }
